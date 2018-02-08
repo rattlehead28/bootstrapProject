a4edd06a883f31bd6151ada42c58668d0398232f
Kartikeya <kartikeya.mudgal28@gmail.com>
2018-02-08 11:27:09 +0530

    bootstrap

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+$('#usr1').click(function(){
+	$(this).css('background-color','orange');
+	$('#companyPosition').html("John Doe / CEO of LoremIpsum");
+	$('#usr2').css('background-color','white');
+	$('#usr3').css('background-color','white');
+
+});
+
+$('#usr2').click(function(){
+	$(this).css('background-color','orange');
+	$('#companyPosition').html("Vito Corleone / CTO of LoremIpsum");
+	$('#usr1').css('background-color','white');
+	$('#usr3').css('background-color','white');
+
+});
+
+$('#usr3').click(function(){
+	$(this).css('background-color','orange');
+	$('#companyPosition').html("Michael Corleone / CFO of LoremIpsum");
+	$('#usr1').css('background-color','white');
+	$('#usr2').css('background-color','white');
+
+});
+});
